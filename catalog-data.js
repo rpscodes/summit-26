c96dc0a1fef20353001d608be4d519fe2d714dde
kcosta123 <kcosta@redhat.com>
2026-05-08 08:43:31 -0400

    Update catalog-data.js

--- catalog-data.js
+++ catalog-data.js
@@ -75,7 +75,7 @@ const catalogData = [
     {
         id: 10,
         title: "Camel, Kaoto, Mapper, AI",
-        category: "Application Foundations",
+        category: "Integration",
         contentType: "arcade",
         imageUrl: "images/runtimes_rb.png",
         link: "https://www.redhat.com/architect/portfolio/detail/75-kaoto-apache-camel-integration-designer-demo"
