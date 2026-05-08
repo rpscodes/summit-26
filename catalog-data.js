8126d72d536c22aef94e53f8c41a734f202b977a
kcosta123 <kcosta@redhat.com>
2026-05-08 08:45:13 -0400

    Update catalog-data.js

--- catalog-data.js
+++ catalog-data.js
@@ -75,7 +75,7 @@ const catalogData = [
     {
         id: 10,
         title: "Camel, Kaoto, Mapper, AI",
-        category: "Integration",
+        category: "Application Foundations",
         contentType: "arcade",
         imageUrl: "images/runtimes_rb.png",
         link: "https://www.redhat.com/architect/portfolio/detail/75-kaoto-apache-camel-integration-designer-demo"
@@ -99,7 +99,7 @@ const catalogData = [
     {
         id: 13,
         title: "Event Streaming and Real Time Data Processing with Red Hat streams for Apache Kafka",
-        category: "Integration",
+        category: "Application Foundations",
         contentType: "demo",
         imageUrl: "images/kafka_rb.png",
         link: "https://interact.redhat.com/share/pYiQKLDlRICTtqM3G8Mw"
@@ -147,7 +147,7 @@ const catalogData = [
     {
         id: 19,
         title: "Red Hat build of Apache Camel",
-        category: "Integration",
+        category: "Application Foundations",
         contentType: "blog",
         imageUrl: "images/camel_rb.png",
         link: "https://developers.redhat.com/products/red-hat-build-of-apache-camel"
